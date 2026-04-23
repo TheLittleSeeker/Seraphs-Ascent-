0b8cf877bc90e0378955b7a8c9eaa643c60ed42f
TheLittleSeeker <Thelittleseeker@hotmail.com>
2026-04-23 14:30:27 +0100

    After Class 23/04/26
    Some stuff and things

---
+++ My project/Assets/Scripts/Respawn.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    public void RespawnAfterDeath()
+    {
+        //transform.position = spawning.position;
+    }
+
+
+
+
+
+}
