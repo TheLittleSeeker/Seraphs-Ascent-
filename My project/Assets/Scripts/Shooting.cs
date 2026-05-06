b2af4605af0f951c353a356a2b8610332fe6c211
TheLittleSeeker <Thelittleseeker@hotmail.com>
2026-05-06 16:18:37 +0100

    After Class 06/05/26
    Added:
    - Beginnings of a shooting system (may need 'total' rework to function how I want)
    - Magic Missile Model and textures (Early ver on textures since Connor needs to add spirals and max/add a logo for it)

--- My project/Assets/Scripts/Shooting.cs
+++ My project/Assets/Scripts/Shooting.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class Shooting : MonoBehaviour
 {
@@ -19,28 +21,36 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector3 rotation = mousePos - transform.position;
-
-        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-
-        transform.rotation = Quaternion.Euler(0, 0, rotZ);
-
-        if (!canFire)
+        var mouse = Mouse.current;
+        if (mouse != null)
         {
-            timer += Time.deltaTime;
-            if (timer > timeBetweenFiring)
-            {
-                canFire = true;
-                timer = 0;
-            }
-
-            if (Input.GetMouseButton(0) && canFire)
-            {
-                canFire = false;
-                Instantiate(bullet, bulletTransform.position, Quaternion.identity);
-            }
+            Vector2 position = mouse.position.ReadValue();
         }
+
+
+        //mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        //Vector3 rotation = mousePos - transform.position;
+
+        //float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        //transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        //if (!canFire)
+        //{
+        //    timer += Time.deltaTime;
+        //    if (timer > timeBetweenFiring)
+        //    {
+        //        canFire = true;
+        //        timer = 0;
+        //    }
+
+        //    if (Input.GetMouseButton(0) && canFire)
+        //    {
+        //        canFire = false;
+        //        Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        //    }
+        //}
     }
 }
